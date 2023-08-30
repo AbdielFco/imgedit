d57860427cf35b47c0477c0726efa80771b77a8d
AbdielFco <88990612+AbdielFco@users.noreply.github.com>
2023-08-30 14:36:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
     const imageInput = document.getElementById("imageInput");
     const editedImage = document.getElementById("editedImage");
-    const filterSliders = document.querySelectorAll(".filter-slider");
+    const brightnessInput = document.getElementById("brightness");
+    const contrastInput = document.getElementById("contrast");
+    const temperatureInput = document.getElementById("temperature");
+    const saturationInput = document.getElementById("saturation");
     const applyFiltersButton = document.getElementById("applyFilters");
+    const cropImageButton = document.getElementById("cropImage");
     const saveImageButton = document.getElementById("saveImage");
-
+  
+    let originalImage = null;
     let currentImage = null;
-
+  
     imageInput.addEventListener("change", function(e) {
       const file = e.target.files[0];
-      if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
-        currentImage = URL.createObjectURL(file);
-        editedImage.src = currentImage;
-      } else {
-        alert("Invalid file format. Please select a valid image (JPEG or PNG).");
-      }
+      originalImage = URL.createObjectURL(file);
+      currentImage = originalImage;
+      editedImage.src = currentImage;
     });
-
+  
     applyFiltersButton.addEventListener("click", applyFilters);
-
-    filterSliders.forEach(slider => {
-      slider.addEventListener("input", function() {
-        applyFilters();
-      });
-    });
-
+    cropImageButton.addEventListener("click", cropImage);
+  
     saveImageButton.addEventListener("click", function() {
       if (currentImage) {
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
-        canvas.width = editedImage.naturalWidth;
-        canvas.height = editedImage.naturalHeight;
-        ctx.filter = getFilterValue();
+        canvas.width = editedImage.width;
+        canvas.height = editedImage.height;
+        ctx.filter = editedImage.style.filter;
         ctx.drawImage(editedImage, 0, 0, canvas.width, canvas.height);
-
+  
         const link = document.createElement("a");
         link.href = canvas.toDataURL("image/jpeg");
         link.download = "edited_image.jpg";
         link.click();
       }
     });
-
+  
     function applyFilters() {
       if (currentImage) {
-        const filterValue = getFilterValue();
-        editedImage.style.filter = filterValue;
+        const brightnessValue = brightnessInput.value;
+        const contrastValue = contrastInput.value;
+        const temperatureValue = temperatureInput.value;
+        const saturationValue = saturationInput.value;
+  
+        editedImage.style.filter = `brightness(${brightnessValue}%) contrast(${contrastValue}%) sepia(${temperatureValue}%) saturate(${saturationValue}%)`;
       }
     }
-
-    function getFilterValue() {
-      const brightnessValue = document.getElementById("brightness").value;
-      const contrastValue = document.getElementById("contrast").value;
-      const temperatureValue = document.getElementById("temperature").value;
-      const saturationValue = document.getElementById("saturation").value;
-
-      return `brightness(${brightnessValue}%) contrast(${contrastValue}%) sepia(${temperatureValue}%) saturate(${saturationValue}%)`;
+  
+    function cropImage() {
+      if (currentImage) {
+        const container = document.querySelector(".image-container");
+        const scaleX = originalImage.width / container.offsetWidth;
+        const scaleY = originalImage.height / container.offsetHeight;
+  
+        const cropX = (container.offsetWidth - editedImage.width) / 2;
+        const cropY = (container.offsetHeight - editedImage.height) / 2;
+  
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        canvas.width = container.offsetWidth;
+        canvas.height = container.offsetHeight;
+  
+        ctx.drawImage(editedImage, cropX * scaleX, cropY * scaleY, editedImage.width, editedImage.height);
+  
+        editedImage.src = canvas.toDataURL("image/jpeg");
+        currentImage = canvas.toDataURL("image/jpeg");
+      }
     }
-});
+  });
